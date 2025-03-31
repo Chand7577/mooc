e37c1bd8158ee70f83cba4a462ae2ab09b5bcf0c
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-03-31 17:15:54 +0530

    Update read_input.py

--- part06/part06-17_read_input/src/read_input.py
+++ part06/part06-17_read_input/src/read_input.py
@@ -1,1 +1,22 @@
 # Write your solution here
+# Write your solution here
+
+
+def read_input(string,num1,num2):
+
+
+    while True:
+        try:
+            typed_num=int(input(f"{string}"))
+            if num1<typed_num<num2:
+                return typed_num
+            else:
+                print(f"You must type in an integer between {num1} and {num2}")
+        except ValueError:
+            print(f"You must type in an integer between {num1} and {num2}")
+            
+
+
+if __name__=="__main__":
+    number = read_input("Please type in a number: ", 5, 10)
+    print(f"You typed in: {number}")

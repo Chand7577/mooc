13b4320af78c084a7cc1b0a4eb6da6ac2c278808
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-03-31 17:16:18 +0530

    Update parameter_validation.py

--- part06/part06-18_parameter_validation/src/parameter_validation.py
+++ part06/part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,23 @@
 # Write your solution here
+# Write your solution here
+
+def new_person(name:str,age:int):
+
+    if name=="":
+        raise ValueError("name cannot be empty!")
+    
+    if len(name.split())<2:
+        raise ValueError("name cannot be less than two words!")
+    
+    if len(name)>40:
+        raise ValueError('name cannot be longer than 40 characters!')
+    
+    if age<0:
+        raise ValueError("Age cannot be negative")
+    if age>150:
+        raise ValueError('Age cannot be greater than 150')
+    
+
+    return (name,age)
+if __name__=="__main__":
+    print(new_person('am',121))

b40410e682c80b195f4ce0a8db6689a22c2ef696
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-03 18:15:39 +0530

    Update calculation_with_fractions.py

--- part07/part07-03_fractions/src/calculation_with_fractions.py
+++ part07/part07-03_fractions/src/calculation_with_fractions.py
@@ -1,1 +1,12 @@
-# Write your solution here
+from fractions import *
+
+
+def fractionate(amount:int):
+    fracs_list=[ Fraction(1,amount) for _ in range(amount)]
+    return fracs_list
+
+   
+if __name__=="__main__":
+    for value in fractionate(3):
+        print(value)
+    print(fractionate(5))

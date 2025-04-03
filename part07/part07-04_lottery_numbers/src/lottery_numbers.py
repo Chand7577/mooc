5d7d7666cd65181a5383ec2a948c69cf7b311416
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-03 18:26:30 +0530

    Update lottery_numbers.py

--- part07/part07-04_lottery_numbers/src/lottery_numbers.py
+++ part07/part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,20 @@
 # Write your solution here
+from random import sample
+
+
+def lottery_numbers(amount:int,lower:int,upper:int):
+    number_pool = list(range(lower,upper))
+   
+    weekly_draw = sample(number_pool, amount)
+    if len(weekly_draw)>1:
+        weekly_draw.sort()
+    
+    return weekly_draw
+
+
+
+
+
+if __name__=="__main__":
+    for value in lottery_numbers(1,1,10):
+        print(value)

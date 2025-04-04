4f0cf4d09801477452e6339aa035fcfd6b8b3f6a
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-04 06:54:30 +0530

    Update password_generator_part_1.py

--- part07/part07-05_password_generator_part_1/src/password_generator_part_1.py
+++ part07/part07-05_password_generator_part_1/src/password_generator_part_1.py
@@ -1,1 +1,12 @@
 # Write your solution here
+from random import sample
+
+def generate_password(number:int):
+    alphabets=[chr(letter) for letter in range(97,123)]
+
+    password=sample(alphabets,number)
+    return "".join(password)
+
+if __name__=="__main__":
+    for _ in range(10):
+        print(generate_password(8))

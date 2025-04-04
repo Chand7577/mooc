3784f230530872d28b46c495f8b9aa6abe8c5c1c
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-04 13:07:21 +0530

    Update random_words.py

--- part07/part07-08_random_words/src/random_words.py
+++ part07/part07-08_random_words/src/random_words.py
@@ -1,1 +1,28 @@
-# Write your solution here
+
+from random import sample
+def getWords(n:int,word_list:list,start:str):
+
+    return sample(word_list,n)
+
+
+
+def words(n: int, beginning: str):
+    word_list = []
+
+    with open('words.txt') as file:
+        for word in file:
+            if word.startswith(beginning):
+                word_list.append(word.strip())
+  
+    
+    
+    if len(word_list)<n:
+        raise ValueError("Couldnt'find words atleast  the expected length ")
+    
+    else:
+        return getWords(n,word_list,beginning)
+
+if __name__=="__main__":
+    word_list=words(2,"ca")
+    for word in word_list:
+        print(word)

3019b04abc02efbe7e08f5d23c931ebadcc15291
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-06 16:55:34 +0530

    Update how_old.py

--- part07/part07-09_how_old/src/how_old.py
+++ part07/part07-09_how_old/src/how_old.py
@@ -1,1 +1,22 @@
 # Write your solution here
+from datetime import datetime
+
+
+def check_year(year:int):
+    return True if year<2000 else False
+
+
+
+day=int(input("Day:"))
+month=int(input("Month:"))
+year=int(input("Year:"))
+user_dob=datetime(year,month,day)
+
+eve_millennium=datetime(1999,12,31)
+
+
+if check_year(year):
+    print(f"You were {(eve_millennium-user_dob).days} days old on the eve of the new millennium.")
+
+else:
+    print(f"You weren't born yet on the eve of the new millennium.")

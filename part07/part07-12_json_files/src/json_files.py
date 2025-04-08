d6d005265da86e1b4659cf368556ccb2ecfe68e2
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-08 13:11:35 +0530

    Update json_files.py

--- part07/part07-12_json_files/src/json_files.py
+++ part07/part07-12_json_files/src/json_files.py
@@ -1,1 +1,26 @@
 # Write your solution here
+
+import json
+
+
+def print_persons(filename:str):
+    with open(filename) as file:
+        data=file.read()
+
+
+    students_info=json.loads(data)
+
+
+    for student in students_info:
+        
+        name,age=student["name"],student["age"]
+        hobbies=student["hobbies"]
+        
+        print(f"{name} {age} years ({', '.join(hobbies)})")
+
+
+if __name__=="__main__":
+    print_persons("file1.json")
+
+
+    

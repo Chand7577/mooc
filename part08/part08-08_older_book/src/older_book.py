06f91c4d2b1ab9a3ad97efb33e80c286fef9dc0f
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-11 23:16:23 +0530

    Update older_book.py

--- part08/part08-08_older_book/src/older_book.py
+++ part08/part08-08_older_book/src/older_book.py
@@ -11,3 +11,23 @@ class Book:
 # -----------------------------
 # Write your solution here
 # -----------------------------
+
+def older_book(book1:Book,book2:Book):
+
+    if book1.year>book2.year:
+        print(f"{book2.name} is older, it was publised in {book2.year}")
+
+    elif book2.year>book1.year:
+        print(f"{book1.name} is older, it was publised in {book1.year}")
+
+    else:
+        print(f"{book1.name} and {book2.name} were published in the {book1.year}")
+
+
+if __name__=="__main__":
+    python = Book("Fluent Python", "Luciano Ramalho", "programming", 2015)
+    everest = Book("High Adventure", "Edmund Hillary", "autobiography", 1956)
+    norma = Book("Norma", "Sofi Oksanen", "crime", 2015)
+
+    older_book(python, everest)
+    older_book(python, norma)

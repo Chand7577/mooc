b141faf7e9e8af9ad800f864ccdd250a9141bf1f
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-11 23:34:50 +0530

    Update decreasing_counter.py

--- part08/part08-10_decreasing_counter/src/decreasing_counter.py
+++ part08/part08-10_decreasing_counter/src/decreasing_counter.py
@@ -2,11 +2,34 @@
 class DecreasingCounter:
     def __init__(self, initial_value: int):
         self.value = initial_value
+        self.state=initial_value
 
     def print_value(self):
         print("value:", self.value)
 
     def decrease(self):
-        pass
+        if self.value>0:
+            self.value-=1
+        
+        elif self.value==0:
+            self.value=0
+
+        return self.value
+
+    def set_to_zero(self):
+        self.value=0
+
+
+    def reset_original_value(self):
+        self.value=self.state
 
     # Write the rest of the methods here!
+if __name__=="__main__":
+    counter = DecreasingCounter(55)
+    counter.decrease()
+    counter.decrease()
+    counter.decrease()
+    counter.decrease()
+    counter.print_value()
+    counter.reset_original_value()
+    counter.print_value()

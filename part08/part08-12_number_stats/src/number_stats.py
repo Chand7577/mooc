80dc4d76cb00864d0e6810ddeb8887b8ef04bbb0
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-12 00:22:08 +0530

    Update number_stats.py

--- part08/part08-12_number_stats/src/number_stats.py
+++ part08/part08-12_number_stats/src/number_stats.py
@@ -2,9 +2,49 @@
 class  NumberStats:
     def __init__(self):
         self.numbers = 0
+        self.total=0
 
     def add_number(self, number:int):
-        pass
+        self.total+=number
+        self.numbers+=1
 
     def count_numbers(self):
-        pass
+        return self.numbers
+
+    def get_sum(self):
+        
+        return self.total
+
+    def average(self):
+        if self.numbers==0:
+            return 0
+        return self.total/self.numbers
+
+
+    
+
+stats = NumberStats()
+even_numbers=NumberStats()
+odd_numbers=NumberStats()
+print(f"Please type in integer numbers:")
+
+while True:
+    
+    input_value=int(input())
+    if input_value==-1:
+        break
+
+    
+    stats.add_number(input_value)
+    if input_value%2==0:
+        even_numbers.add_number(input_value)
+
+    else:
+        odd_numbers.add_number(input_value)
+    
+    
+    
+print(f"Sum of numbers: {stats.get_sum()}")
+print(f"Mean of numbers: {stats.average()}")
+print(f"Sum of even numbers: {even_numbers.get_sum()}")
+print(f"Sum of odd numbers: {odd_numbers.get_sum()}")

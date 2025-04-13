74caaa24c7228ac73aedd68e4d196d7eecefc6e4
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-13 11:38:52 +0530

    Update pets.py

--- part09/part09-06_pets/src/pets.py
+++ part09/part09-06_pets/src/pets.py
@@ -14,4 +14,12 @@ class Person:
         self.pet = pet
 
     def __str__(self):
-        return self.name
+        return f"{self.name}, whose pal is {self.pet.name}, a {self.pet.description}"
+
+
+
+if __name__=="__main__":
+    hulda = Pet("Buddy", "little dog")
+    levi = Person("Peter", hulda)
+
+    print(levi)

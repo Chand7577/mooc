69ca2b82bab2678f3d59a69892400ad1c516f7ad
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-13 11:39:56 +0530

    Update box_of_presents.py

--- part09/part09-07_box_of_presents/src/box_of_presents.py
+++ part09/part09-07_box_of_presents/src/box_of_presents.py
@@ -1,1 +1,35 @@
 # WRITE YOUR SOLUTION HERE:
+
+class Present:
+    def __init__(self,name,weight):
+        self.name=name
+        self.weight=weight
+
+    def __str__(self):
+        return f"Present:{self.name} ({self.weight})"
+
+
+
+class Box :
+    def __init__(self):
+        self.box=[]
+        self.weight=0
+
+    def add_present(self,present:Present):
+        self.weight+=present.weight
+        self.box.append(present)
+
+    def  total_weight(self):
+        return self.weight
+
+
+if __name__=="__main__":
+    book = Present("ABC Book", 2)
+
+    box = Box()
+    box.add_present(book)
+    print(box.total_weight())
+
+    cd = Present("Pink Floyd: Dark Side of the Moon", 1)
+    box.add_present(cd)
+    print(box.total_weight())

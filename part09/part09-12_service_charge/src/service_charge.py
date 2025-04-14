2800d08e216a95d3aead6313d206f6d8f7fb7beb
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-14 10:30:36 +0530

    Update service_charge.py

--- part09/part09-12_service_charge/src/service_charge.py
+++ part09/part09-12_service_charge/src/service_charge.py
@@ -1,3 +1,37 @@
 # WRITE YOUR SOLUTION HERE:
+class BankAccount:
+
+    def __init__(self,owner:str,account_no:str,bal:float):
+        self.__owner=owner
+        self.__account_no=account_no
+        self.__bal=bal
+
+    def deposit(self,amount:float):
+        if amount>0:
+            self.__bal+=amount
+            self.__service_charge()
+
+        
+    
+    def withdraw(self,amount:float):
+        if self.__bal>amount:
+            self.__bal-=amount
+            self.__service_charge()
+
+    @property
+    def balance(self):
+        return self.__bal
 
 
+    
+    def __service_charge(self):
+        self.__bal-=0.01*self.__bal
+
+
+if __name__=="__main__":
+    account = BankAccount("Randy Riches", "12345-6789", 1000)
+
+    account.withdraw(100)
+    print(account.balance)
+    account.deposit(100)
+    print(account.balance)

6da36e6fca12f63b6dc99445152aae6c2b7af5ee
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-14 09:11:39 +0530

    Update car.py

--- part09/part09-09_car/src/car.py
+++ part09/part09-09_car/src/car.py
@@ -1,1 +1,42 @@
 # WRITE YOUR SOLUTION HERE:
+
+
+class Car:
+
+    def __init__(self):
+        self.__petrol=0
+        self.__reading=0
+
+
+
+    def fill_up(self):
+        if self.__petrol==0:
+            self.__petrol=60
+    
+    def drive(self,km:int):
+        
+        if self.__petrol >= km:
+            self.__reading += km
+            self.__petrol -= km
+        else:
+            self.__reading += self.__petrol
+            self.__petrol = 0
+
+
+    def __str__(self):
+        return f"Car: odometer reading {self.__reading} km, petrol remaining {self.__petrol} litres"
+
+if __name__=="__main__":
+    car = Car()
+    print(car)
+    car.fill_up()
+    print(car)
+    car.drive(20)
+    print(car)
+    car.drive(50)
+    print(car)
+    car.drive(10)
+    print(car)
+    car.fill_up()
+    car.fill_up()
+    print(car)

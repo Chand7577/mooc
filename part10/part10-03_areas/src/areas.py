c042b3ea91cb6e1368abca871792f4a0c906e4d9
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-14 22:48:59 +0530

    Update areas.py

--- part10/part10-03_areas/src/areas.py
+++ part10/part10-03_areas/src/areas.py
@@ -9,3 +9,22 @@ class Rectangle:
 
     def area(self):
         return self.width * self.height
+
+
+class Square(Rectangle):
+    def __init__(self,side:int):
+        super().__init__(side,side)
+
+
+    def __str__(self):
+         return f"square {self.width}x{self.height}"
+
+
+    def area(self):
+        return super().area()
+
+
+if __name__=="__main__":
+    square = Square(4)
+    print(square)
+    print("area:", square.area())

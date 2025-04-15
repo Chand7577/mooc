541b007bd24285f8172b8b5e8db02265628c48fb
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-15 10:53:32 +0530

    Update secret_magic_potion.py

--- part10/part10-06_secret_magic_potion/src/secret_magic_potion.py
+++ part10/part10-06_secret_magic_potion/src/secret_magic_potion.py
@@ -1,15 +1,47 @@
 # Write your solution here:
-class MagicPotion:
-    def __init__(self, name: str):
-        self._name = name
-        self._ingredients = []
+class SuperHero:
+    def __init__(self, name: str, superpowers: str):
+        self.name = name
+        self.superpowers = superpowers
 
-    def add_ingredient(self, ingredient: str, amount: float):
-        self._ingredients.append((ingredient, amount))
-
-    def print_recipe(self):
-        print(self._name + ":")
-        for ingredient in self._ingredients:
-            print(f"{ingredient[0]} {ingredient[1]} grams")
+    def __str__(self):
+        return f'{self.name}, superpowers: {self.superpowers}'
 
 
+class SuperGroup:
+    def __init__(self,name:str,location:str):
+        self._name=name
+        self._location=location
+        self._members=[]
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+
+    def location(self):
+        return self._location
+
+    
+    def add_member(self,hero:SuperHero):
+        self._members.append(hero)
+
+
+    def print_group(self):
+        print(f"{self.name}, {self.location}")
+        print("Members:")
+
+        for hero in self._members:
+            print(f"{hero.name}, superpowers: {hero.superpowers}")
+
+
+
+if __name__=="__main__":
+    superperson = SuperHero("SuperPerson", "Superspeed, superstrength")
+    invisible = SuperHero("Invisible Inca", "Invisibility")
+    revengers = SuperGroup("Revengers", "Emerald City")
+
+    revengers.add_member(superperson)
+    revengers.add_member(invisible)
+    revengers.print_group()

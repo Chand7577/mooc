6a641cc4bd530ce65d864b1049057150d3ce815e
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-19 10:39:33 +0530

    Update most_common_words.py

--- part11/part11-12_most_common_words/src/most_common_words.py
+++ part11/part11-12_most_common_words/src/most_common_words.py
@@ -1,1 +1,12 @@
 # WRITE YOUR SOLUTION HERE:
+
+def most_common_words(filename:str,lower_limit:int):
+    data = open(filename).read().replace('\n', ' ').replace('.', '').replace(',', ' ').replace('  ', ' ')
+    data=data.split()
+
+    return {word:data.count(word) for word in data if data.count(word)>=lower_limit}
+               
+                
+if __name__=="__main__":
+
+    print(most_common_words("comprehensions.txt",3))

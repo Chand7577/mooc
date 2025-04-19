a34440102f24ef92744387b35b4f1d8ea9feedf2
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-19 11:43:08 +0530

    Update balanced_brackets.py

--- part11/part11-15_balanced_brackets/src/balanced_brackets.py
+++ part11/part11-15_balanced_brackets/src/balanced_brackets.py
@@ -1,9 +1,30 @@
 
 def balanced_brackets(my_string: str):
+    new_string=""
+    for char in my_string:
+        if char in "[]()":
+            new_string+=char
+
+
+    my_string=new_string
+
+
     if len(my_string) == 0:
         return True
-    if not (my_string[0] == '(' and my_string[-1] == ')'):
-        return False
-
+    
+    if my_string[0]=="(":
+        if not (my_string[0] == '(' and my_string[-1] == ')'):
+            return False
+    else:
+        if not (my_string[0]=='[' and my_string[-1]==']'):
+        
+            return False
     # remove first and last character
     return balanced_brackets(my_string[1:-1])
+
+
+
+if __name__=="__main__":
+    ok = balanced_brackets("(python version [3.7]) please use this one!")
+    print(ok)
+

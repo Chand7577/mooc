0fe73c45de61b128486c7b8e9383fe1a69b2c2c6
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-21 13:32:34 +0530

    Update prime_numbers.py

--- part12/part12-09_prime_numbers/src/prime_numbers.py
+++ part12/part12-09_prime_numbers/src/prime_numbers.py
@@ -1,1 +1,22 @@
 # Write your solution here
+
+def prime_numbers():
+    starting=2
+    while True:
+        divisible=False
+        for value in range(2,starting-1):
+            if starting%value==0:
+                divisible=True
+                break
+
+        if not divisible:
+            yield starting
+
+        starting+=1
+
+
+if __name__=="__main__":
+    numbers=prime_numbers()
+    for i in range(8):
+        print(next(numbers))
+        

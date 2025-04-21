7184226b50d983cd0be6c426cd13263d9872238a
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-21 13:32:58 +0530

    Update random_words.py

--- part12/part12-10_random_words/src/random_words.py
+++ part12/part12-10_random_words/src/random_words.py
@@ -1,1 +1,11 @@
 # Write your solution here:
+import random
+def word_generator(characters:str,length:int ,amount:int):
+    for _ in range(amount):
+        yield "".join(random.sample(characters,length))
+
+
+if __name__=="__main__":
+    wordgen = word_generator("abcdefg", 3, 5)
+    for word in wordgen:
+        print(word)

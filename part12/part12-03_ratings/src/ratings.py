8445976839d3d643a4ffd97c13c168eb83137194
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-21 11:35:51 +0530

    Update ratings.py

--- part12/part12-03_ratings/src/ratings.py
+++ part12/part12-03_ratings/src/ratings.py
@@ -1,1 +1,19 @@
-# TEE RATKAISUSI TÄHÄN:
+
+# Write your solution here:
+def order_by_ratings(programme:dict):
+    return programme['rating']
+
+
+
+def sort_by_ratings(shows:list):
+
+    sorted_by_rat=sorted(shows,key=order_by_ratings,reverse=True)
+    return sorted_by_rat
+
+
+
+if __name__=="__main__":
+    shows = [{ "name": "Dexter", "rating" : 8.6, "seasons":9 }, { "name": "Friends", "rating" : 8.9, "seasons":10 },  { "name": "Simpsons", "rating" : 8.7, "seasons":32 }  ]
+    print("Rating according to IMDB")
+    for show in sort_by_ratings(shows):
+        print(f"{show['name']}  {show['rating']}")

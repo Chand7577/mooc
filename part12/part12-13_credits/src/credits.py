c8cda42ab662b384ec5d2331b775d7fd445eaf30
Amrit <100637966+Chand7577@users.noreply.github.com>
2025-04-22 11:22:15 +0530

    Update credits.py

--- part12/part12-13_credits/src/credits.py
+++ part12/part12-13_credits/src/credits.py
@@ -10,3 +10,29 @@ class CourseAttempt:
         return f"{self.course_name} ({self.credits} cr) grade {self.grade}"
 
 # Write your solution
+
+
+def sum_of_all_credits(credits:list):
+
+    result=reduce(lambda reducedSum,course:reducedSum+course.credits,credits,0)
+    return result
+
+def sum_of_passed_credits(credits:list):
+    filteredlist=list(filter(lambda course:course.grade>=1,credits))
+    result=reduce(lambda reducedSum,course:reducedSum+course.credits,filteredlist,0)
+    return result
+
+
+def average(credits:list):
+    filteredlist=list(filter(lambda course:course.grade>=1,credits))
+    result=reduce(lambda reducedSum,course:reducedSum+course.grade,filteredlist,0)
+    return result/len(filteredlist)
+
+if __name__=="__main__":
+    s1 = CourseAttempt("Introduction to Programming", 5, 5)
+    s2 = CourseAttempt("Advanced Course in Programming", 0, 4)
+    s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    
+
+    credit_sum = sum_of_all_credits([s1, s2, s3])
+    print(credit_sum)
